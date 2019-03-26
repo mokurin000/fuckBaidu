6719aa3372fabd80c2d2001e1fa0b4e684b436b9
poly000 <34085039+poly000@users.noreply.github.com>
2019-03-26 23:37:14 +0800

    Add files via upload

---
+++ continue.bat
@@ -0,0 +1,4 @@
+BaiduPCS-Go config set -appid=265486
+BaiduPCS-Go d /apps/baidu_shurufa/*
+BaiduPCS-Go rm /apps/baidu_shurufa/*
+BaiduPCS-Go config set -appid=266719
---
+++ download.bat
@@ -0,0 +1,10 @@
+﻿@echo off
+echo 请检索文件路径
+BaiduPCS-Go config set -appid=266719
+BaiduPCS-Go web
+set /p file=
+BaiduPCS-Go cp "%file%" /apps/baidu_shurufa
+BaiduPCS-Go config set -appid=265486
+BaiduPCS-Go d /apps/baidu_shurufa/*
+BaiduPCS-Go rm /apps/baidu_shurufa/*
+BaiduPCS-Go config set -appid=266719
