536e5b0217c975974b102c26319c2cbde9b94027
poly000 <34085039+poly000@users.noreply.github.com>
2019-04-05 15:22:34 +0800

    Update BaiduPCS-Go_Download.sh

--- BaiduPCS-Go_Download.sh
+++ BaiduPCS-Go_Download.sh
@@ -7,17 +7,13 @@ then	BaiduPCS-Go config set -appid=266719 &> /dev/null
 	BaiduPCS-Go d /apps/baidu_shurufa/*
 	BaiduPCS-Go rm /apps/baidu_shurufa/* &> /dev/null
 	BaiduPCS-Go config set -appid=266719 &> /dev/null
-else	cat<<QWQ
-使用方法： $0 文件路径
-QWQ
-	if [ ! -f $HOME/exist_dir ]
-	then	if [ -f $HOME/BaiduPCS-Go/BaiduPCS-Go ]
-		then	ln -s $HOME/BaiduPCS-Go/BaiduPCS-Go $PREFIX/bin/BaiduPCS-Go
-		fi
-		BaiduPCS-Go config set -appid=266719 &> /dev/null
-		BaiduPCS-Go mkdir /apps/baidu_shurufa &> /dev/null
-		echo 所需文件夹已建立！
-		touch exist_dir
+else	echo 使用方法： $0 文件路径
+	if [ ! -f ~/exist_dir ]
+	BaiduPCS-Go config set -appid=266719 &> /dev/null
+	BaiduPCS-Go mkdir /apps/baidu_shurufa &> /dev/null
+	echo 所需文件夹已建立！
+	touch ~/exist_dir
 	fi
+	echo 请创建链接或使/usr/bin/BaiduPCS-Go链接到二进制可执行文件
 fi
 BaiduPCS-Go config set -appid=266719 &> /dev/null
