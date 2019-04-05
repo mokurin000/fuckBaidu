0a9e70ac04cc71fb125798f1b6144351287d81cb
poly000 <34085039+poly000@users.noreply.github.com>
2019-04-05 15:34:53 +0800

    Update BaiduPCS-Go_Download.sh

--- BaiduPCS-Go_Download.sh
+++ BaiduPCS-Go_Download.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-if [ -f /usr/bin/BaiduPCS-Go ]&&[ o$* != o ]
+function BaiduPCS-Go(){ BaiduPCS-Go $@ } #修改BaiduPCS-Go为可执行文件路径即可
+
+if [ o$* != o ]
 then	BaiduPCS-Go config set -appid=266719 &> /dev/null
 	BaiduPCS-Go cp "${@}" /apps/baidu_shurufa &> /dev/null
 	BaiduPCS-Go config set -appid=265486 &> /dev/null
@@ -14,6 +16,5 @@ else	echo 使用方法： $0 文件路径
 		echo 所需文件夹已建立！
 		touch ~/.exist_dir
 	fi
-	echo 请创建链接或使/usr/bin/BaiduPCS-Go链接到二进制可执行文件
 fi
 BaiduPCS-Go config set -appid=266719 &> /dev/null
