45d85b3132fe41b662cd1b4bd1339e2ea186e4f0
poly000 <34085039+poly000@users.noreply.github.com>
2019-05-20 12:26:44 +0800

    Update BaiduPCS-Go_Download.sh

--- BaiduPCS-Go_Download.sh
+++ BaiduPCS-Go_Download.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 alias BaiduPCS-Go=BaiduPCS-Go #修改右边的BaiduPCS-Go为可执行文件路径即可
-if [ o$* != o-h ]&&[ o$* != o--help ]&&[ o$* != o ]
+if [ o"$*" != o-h ]&&[ o"$*" != o--help ]&&[ o"$*" != o ]
 then	BaiduPCS-Go config set -appid=266719 &> /dev/null
-	while BaiduPCS-Go cp $* /apps/baidu_shurufa |grep 31045 &> /dev/null
+	while BaiduPCS-Go cp "$*" /apps/baidu_shurufa |grep 31045 &> /dev/null
 	do :
 	done
 	BaiduPCS-Go config set -appid=265486 &> /dev/null
-	while BaiduPCS-Go d /apps/baidu_shurufa/*|tee 0.txt&&grep 31045 0.txt &> /dev/null
+	while BaiduPCS-Go d /apps/baidu_shurufa/\*|tee 0.txt&&grep 31045 0.txt &> /dev/null
 	do :
 	done
-	while BaiduPCS-Go rm /apps/baidu_shurufa/* |grep 31045 &> /dev/null
+	while BaiduPCS-Go rm /apps/baidu_shurufa/\* |grep 31045 &> /dev/null
 	do :
 	done
 	BaiduPCS-Go config set -appid=266719 &> /dev/null
