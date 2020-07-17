cac83fa2dbc305b0d683cfd6d8a7f99caffd55b4
poly000 <34085039+poly000@users.noreply.github.com>
2020-07-18 07:28:31 +0800

    Delete BaiduPCS-Go_Download.bat

--- BaiduPCS-Go_Download.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-BaiduPCS-Go config set -appid=266719
-BaiduPCS-Go web
-set /p file=
-BaiduPCS-Go cp "%file%" /apps/baidu_shurufa
-BaiduPCS-Go config set -appid=265486
-BaiduPCS-Go d /apps/baidu_shurufa/*
-BaiduPCS-Go rm /apps/baidu_shurufa/*
-BaiduPCS-Go config set -appid=266719
